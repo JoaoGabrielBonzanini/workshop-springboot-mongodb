a8b0f58523a3e49e7f348007e1450df00e712ddb
JoaoGabrielBonzanini <joaogabrielbonzanini@gmail.com>
2025-03-26 10:58:57 -0300

    AuthorDTO with data

--- src/main/java/com/mongodb/MongoDB/config/Instantation.java
+++ src/main/java/com/mongodb/MongoDB/config/Instantation.java
@@ -2,6 +2,7 @@ package com.mongodb.MongoDB.config;
 
 import com.mongodb.MongoDB.domain.Post;
 import com.mongodb.MongoDB.domain.User;
+import com.mongodb.MongoDB.dto.AuthorDTO;
 import com.mongodb.MongoDB.repositories.PostRepository;
 import com.mongodb.MongoDB.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("24/03/2025"), "Partiu viagem!", "Estou indo viajar", maria);
-        Post post2 = new Post(null, sdf.parse("25/03/2025"), "Um belo dia", "Dia de chuva!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("24/03/2025"), "Partiu viagem!", "Estou indo viajar", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("25/03/2025"), "Um belo dia", "Dia de chuva!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
