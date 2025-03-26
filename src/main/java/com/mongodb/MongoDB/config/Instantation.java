d03f9b05b59b47e1e81833600213ee4d53be12d3
JoaoGabrielBonzanini <joaogabrielbonzanini@gmail.com>
2025-03-26 12:35:32 -0300

    Added comments in the posts

--- src/main/java/com/mongodb/MongoDB/config/Instantation.java
+++ src/main/java/com/mongodb/MongoDB/config/Instantation.java
@@ -3,6 +3,7 @@ package com.mongodb.MongoDB.config;
 import com.mongodb.MongoDB.domain.Post;
 import com.mongodb.MongoDB.domain.User;
 import com.mongodb.MongoDB.dto.AuthorDTO;
+import com.mongodb.MongoDB.dto.CommentDTO;
 import com.mongodb.MongoDB.repositories.PostRepository;
 import com.mongodb.MongoDB.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.TimeZone;
 
 @Configuration
@@ -40,6 +42,13 @@ public class Instantation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("24/03/2025"), "Partiu viagem!", "Estou indo viajar", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("25/03/2025"), "Um belo dia", "Dia de chuva!", new AuthorDTO(maria));
+
+        CommentDTO comment1 = new CommentDTO("Faça uma boa viagem", sdf.parse("24/03/2025"), new AuthorDTO(alex));
+        CommentDTO comment2 = new CommentDTO("Divirta-se", sdf.parse("24/03/2025"), new AuthorDTO(bob));
+        CommentDTO comment3 = new CommentDTO("Sempre bom chuva", sdf.parse("25/03/2025"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(comment1, comment2));
+        post2.getComments().add(comment3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
