86823bd76d42eba7daab01bd7de8bf2dace54876
JoaoGabrielBonzanini <joaogabrielbonzanini@gmail.com>
2025-04-09 09:33:44 -0300

    Returning simple query methods

--- src/main/java/com/mongodb/MongoDB/resources/PostResource.java
+++ src/main/java/com/mongodb/MongoDB/resources/PostResource.java
@@ -2,14 +2,14 @@ package com.mongodb.MongoDB.resources;
 
 
 import com.mongodb.MongoDB.domain.Post;
+import com.mongodb.MongoDB.resources.util.URL;
 import com.mongodb.MongoDB.services.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URLDecoder;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -23,5 +23,12 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
